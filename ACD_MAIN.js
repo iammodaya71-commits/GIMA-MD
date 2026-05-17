c37505d7d98933ec82d9a25650eb05e1c7d5eb81
iammodaya71-commits <iammodaya71@gmail.com>
2026-05-17 10:57:34 +0530

    Update ACD_MAIN.js

--- ACD_MAIN.js
+++ ACD_MAIN.js
@@ -1,22 +1,17 @@
-const config = require('../config')
-const { cmd, commands } = require('../command')
+if (text == ".alive") {
+await sock.sendMessage(from, {
+text: "GIMA MD IS ONLINE ✅"
+}, { quoted: mek })
+}
 
-cmd({
-    pattern: "ping",
-    desc: "Check bot's response time.",
-    category: "main",
-    react: "✅",
-    filename: __filename
-},
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '*GIMA-md...*' })
-        const endTime = Date.now()
-        const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*📍 Ping : ${ping}ms*` }, { quoted: message })
-    } catch (e) {
-        console.log(e)
-        reply(`${e}`)
-    }
-})
+if (text == ".owner") {
+await sock.sendMessage(from, {
+text: "OWNER : PAWAN\nNUMBER : 94762964170"
+}, { quoted: mek })
+}
+
+if (text == ".ping") {
+await sock.sendMessage(from, {
+text: "Pong 🏓"
+}, { quoted: mek })
+}
